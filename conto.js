d5356df5c4d39800b6aff0e945eff95c5e16f95f
ElisaGelmetti <134309337+ElisaGelmetti@users.noreply.github.com>
2023-07-19 09:07:56 +0200

    soluzioni

--- conto.js
+++ conto.js
@@ -1,40 +1,39 @@
 "use strict";
 // creo una classe che gestisce le operazioni
 class BankAccount {
-    constructor() {
-        this.balance = 0;
+  constructor() {
+    this.balance = 0;
+  }
+  // deposito i soldi
+  deposit(amount) {
+    this.balance += amount;
+  }
+  // prelievo
+  withdraw(amount) {
+    if (amount <= this.balance) {
+      this.balance -= amount;
+    } else {
+      console.log("Saldo insufficiente.");
     }
-    // deposito i soldi
-    deposit(amount) {
-        this.balance += amount;
-    }
-    // prelievo
-    withdraw(amount) {
-        if (amount <= this.balance) {
-            this.balance -= amount;
-        }
-        else {
-            console.log("Saldo insufficiente.");
-        }
-    }
+  }
 }
 // creo la classe Sons
 class SonsAccount extends BankAccount {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 }
 // creo la classo Mother
 class MotherAccount extends BankAccount {
-    constructor(interestRate) {
-        super();
-        this.interestRate = interestRate;
-    }
-    // interessi
-    calculateInterest() {
-        const interest = this.balance * (this.interestRate / 100);
-        this.deposit(interest);
-    }
+  constructor(interestRate) {
+    super();
+    this.interestRate = interestRate;
+  }
+  // interessi
+  calculateInterest() {
+    const interest = this.balance * (this.interestRate / 100);
+    this.deposit(interest);
+  }
 }
 // Utilizzo delle classi per eseguire operazioni sui conti correnti
 const sonsAccount = new SonsAccount();
@@ -45,3 +44,91 @@ motherAccount.deposit(2000);
 motherAccount.calculateInterest();
 console.log("Saldo conto figlio:", sonsAccount.balance);
 console.log("Saldo conto madre:", motherAccount.balance);
+
+//SOLUZIONE
+// var __extends =
+//   (this && this.__extends) ||
+//   (function () {
+//     var extendStatics = function (d, b) {
+//       extendStatics =
+//         Object.setPrototypeOf ||
+//         ({ __proto__: [] } instanceof Array &&
+//           function (d, b) {
+//             d.__proto__ = b;
+//           }) ||
+//         function (d, b) {
+//           for (var p in b)
+//             if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p];
+//         };
+//       return extendStatics(d, b);
+//     };
+//     return function (d, b) {
+//       extendStatics(d, b);
+//       function __() {
+//         this.constructor = d;
+//       }
+//       d.prototype =
+//         b === null
+//           ? Object.create(b)
+//           : ((__.prototype = b.prototype), new __());
+//     };
+//   })();
+// var SonAccount = /** @class */ (function () {
+//   function SonAccount(balanceInit) {
+//     this.balanceInit = balanceInit;
+//   }
+//   SonAccount.prototype.deposit = function (amount) {
+//     return amount;
+//   };
+//   SonAccount.prototype.oneWithdraw = function (amount) {
+//     return amount;
+//   };
+//   SonAccount.prototype.twoWithdraw = function (amount) {
+//     return amount;
+//   };
+//   SonAccount.prototype.totalBalance = function () {
+//     return this.oneWithdraw(500) - this.twoWithdraw(400);
+//   };
+//   return SonAccount;
+// })();
+// console.log("------------son account----------------- " + "\n");
+// var son = new SonAccount(0);
+// console.log(son);
+// console.log("deposit:+" + son.deposit(1000));
+// console.log("withdraw:-" + son.oneWithdraw(500));
+// console.log("withdraw:-" + son.twoWithdraw(400));
+// console.log("totalBalance:" + son.totalBalance());
+// var MotherAccount = /** @class */ (function (_super) {
+//   __extends(MotherAccount, _super);
+//   function MotherAccount(balanceInit) {
+//     return _super.call(this, balanceInit) || this;
+//   }
+//   MotherAccount.prototype.deposit = function (amount) {
+//     return amount;
+//   };
+//   MotherAccount.prototype.oneWithdraw = function (amount) {
+//     return amount;
+//   };
+//   MotherAccount.prototype.twoWithdraw = function (amount) {
+//     return amount;
+//   };
+//   MotherAccount.prototype.getBalance = function () {
+//     return this.oneWithdraw(500) - this.twoWithdraw(400);
+//   };
+//   MotherAccount.prototype.addInterest = function (rate) {
+//     return (this.getBalance() * rate) / 100;
+//   };
+//   MotherAccount.prototype.totalBalance = function () {
+//     return this.getBalance() - this.addInterest(10);
+//   };
+//   return MotherAccount;
+// })(SonAccount);
+// var mother = new MotherAccount(0);
+// console.log(
+//   "--------------mother account----------------------------------" + "\n"
+// );
+// console.log(mother);
+// console.log("deposit:+" + mother.deposit(1000));
+// console.log("withdraw:-" + mother.oneWithdraw(500));
+// console.log("withdraw:-" + mother.twoWithdraw(400));
+// console.log("totalBalance:" + mother.totalBalance());
--- conto.ts
+++ conto.ts
@@ -43,11 +43,103 @@ class MotherAccount extends BankAccount {
 // Utilizzo delle classi per eseguire operazioni sui conti correnti
 const sonsAccount = new SonsAccount();
 const motherAccount = new MotherAccount(5);
-
+// deposito del figlio
 sonsAccount.deposit(1000);
+// prelievo del figlio
 sonsAccount.withdraw(200);
+// deposito della madre
 motherAccount.deposit(2000);
+// calcolo gli interessi
 motherAccount.calculateInterest();
 
 console.log("Saldo conto figlio:", sonsAccount.balance);
 console.log("Saldo conto madre:", motherAccount.balance);
+
+//SOLUZIONE
+
+// class SonAccount {
+//   // nome della classe
+
+//   balanceInit: number; // proprietà di classe
+
+//   constructor(balanceInit: number) {
+//     // costruttore della classe che referenzia la proprietà balanceInit
+
+//     this.balanceInit = balanceInit;
+//   }
+//   // implementazione del metodo deposit
+//   public deposit(amount: number): number {
+//     return amount;
+//   }
+
+//   // implementazione del metodo oneWithdraw
+//   public oneWithdraw(amount: number): number {
+//     return amount;
+//   }
+
+//   // implementazione del metodo twoWithdraw
+//   public twoWithdraw(amount: number): number {
+//     return amount;
+//   }
+
+//   // implementazione del metodo totalBalance
+//   public totalBalance(): number {
+//     return this.oneWithdraw(500) - this.twoWithdraw(400);
+//   }
+// }
+// console.log("------------son account----------------- " + "\n");
+
+// // set della proprietà al costruttore
+// let son = new SonAccount(0);
+// console.log(son);
+
+// // stampo i versamenti-prelievi fatti ed il saldo attuale del conto
+// console.log("deposit:+" + son.deposit(1000));
+// console.log("withdraw:-" + son.oneWithdraw(500));
+// console.log("withdraw:-" + son.twoWithdraw(400));
+// console.log("totalBalance:" + son.totalBalance());
+
+// // classe MotherAccount estende SonAccount
+// class MotherAccount extends SonAccount {
+//   //costruttore referenzia la proprietà
+//   constructor(balanceInit: number) {
+//     super(balanceInit);
+//   }
+
+//   public deposit(amount: number): number {
+//     return amount;
+//   }
+
+//   public oneWithdraw(amount: number): number {
+//     return amount;
+//   }
+
+//   public twoWithdraw(amount: number): number {
+//     return amount;
+//   }
+
+//   public getBalance(): number {
+//     return this.oneWithdraw(500) - this.twoWithdraw(400);
+//   }
+
+//   // implementazione del metodo addInterest
+//   public addInterest(rate: number): number {
+//     return (this.getBalance() * rate) / 100;
+//   }
+
+//   // implementazione del metodo totalBalance
+//   public totalBalance(): number {
+//     return this.getBalance() - this.addInterest(10);
+//   }
+// }
+
+// let mother = new MotherAccount(0);
+// console.log(
+//   "--------------mother account----------------------------------" + "\n"
+// );
+// console.log(mother);
+// // stampo i versamenti-prelievi fatti ed il saldo attuale del conto
+// console.log("deposit:+" + mother.deposit(1000));
+// console.log("withdraw:-" + mother.oneWithdraw(500));
+// console.log("withdraw:-" + mother.twoWithdraw(400));
+// console.log("totalBalance:" + mother.totalBalance());
